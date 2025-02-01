63a7aa154acbb64550684d5cbdebfcfb904ce14a
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-02-01 09:21:31 +0600

    add triangle pattern

--- patterns/patterns.js
+++ patterns/patterns.js
@@ -1,5 +1,6 @@
-process.stdout.write("Print Square: \n");
 const printSquare = (n) => {
+  process.stdout.write("Print Square: \n");
+
   for (let row = 1; row <= n; row++) {
     for (let col = 1; col <= n; col++) {
       process.stdout.write("*");
@@ -8,5 +9,17 @@ const printSquare = (n) => {
   }
 };
 
+const printTriangle = (n) => {
+  process.stdout.write("Print Triangle: \n");
+
+  for (let row = 1; row <= n; row++) {
+    for (let col = 1; col <= row; col++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+};
+
 const n = 5;
 printSquare(n);
+printTriangle(n);
