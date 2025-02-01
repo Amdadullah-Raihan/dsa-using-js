46e8c25efc0d41b337d8f3ffdb3e491134e712bf
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-02-01 13:26:12 +0600

    separate inverted number triangle

---
+++ patterns/invertedNumberTriangle.js
@@ -0,0 +1,11 @@
+const printInvertedTriangle = (n) => {
+  process.stdout.write("Inverted Triangle: \n");
+
+  for (let row = 1; row <= n; row++) {
+    for (let col = 1; col <= n + 1 - row; col++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+};
+
