2b4395f0350d75f3b5a7e2e5870ef2abf45d23c5
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-02-08 11:42:31 +0600

    add a recursive factorial

---
+++ recursion/factorial.js
@@ -0,0 +1,9 @@
+const factorial = (n) => {
+  if (n ==  1 || n ==0) return 1;
+  return  n * factorial(n - 1);
+};
+const n = 0;
+console.log("factorial of", n, "is: ", factorial(10))
+
+
+5 *  4 *  3 * 2 * 1 
