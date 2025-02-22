e6eb1167670606b75767322dd4c022529fdc532a
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-02-22 19:05:39 +0600

    remove duplicates from a sorted array and return the length of reordered arr(in place)

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -1,12 +1,13 @@
-const removeDuplicates = (arr) => {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i + 1]) {
-      newArr.push(arr[i]);
+var removeDuplicates = function (arr) {
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      arr[i + 1] = arr[j];
+      i++;
     }
   }
-  return newArr;
+  return i + 1;
 };
 
-const arr = [0,0,1, 2, 2, 3, 3, 4, 5, 5, 5];
+const arr = [1, 1, 2];
 console.log("without duplicate: ", removeDuplicates(arr));
