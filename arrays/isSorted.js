5319e2cc5c800a8327cd2905161c4dd6f6c842f2
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-02-22 18:12:27 +0600

    refactor isSorted function for improved logic and readability

--- arrays/isSorted.js
+++ arrays/isSorted.js
@@ -1,9 +1,7 @@
 const isSorted = (arr) => {
   for (let i = 1; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i - 1] && arr[i] < arr[i + 1]) {
-      continue;
-    } else {
-      return false;
+    if (arr[i] < arr[i - 1] ) {
+      return   false;
     }
   }
   return true;
