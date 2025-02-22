0886ad4df9b4a2d25e850af53dd65473e162ac26
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-02-22 17:48:40 +0600

    refactor second largest element function and add two-pass solution

--- arrays/secondLargestElm.js
+++ arrays/secondLargestElm.js
@@ -1,6 +1,6 @@
 //Brute force - sort the arr and pick the value of 2nd index(Only works if there are no duplicates)
 
-const secondLargestElement = (arr) => {
+const find2ndLargest_BruteForce = (arr) => {
   const sortedArr = arr.sort((a, b) => a - b);
 
   const largestElm = sortedArr[sortedArr.length - 1];
@@ -10,4 +10,24 @@ const secondLargestElement = (arr) => {
 };
 
 const arr = [6, 3, 1, -6, 10, 7];
-console.log("second largest elm: ", secondLargestElement(arr));
+console.log(
+  "second largest elm(brute force): ",
+  find2ndLargest_BruteForce(arr)
+);
+
+//Better approach with two passes
+const find2ndLargest_TwoPass = (arr) => {
+  let largest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) largest = arr[i];
+  }
+
+  let secondLargest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < largest && arr[i] !== largest) secondLargest = arr[i];
+  }
+
+  return secondLargest;
+};
+
+console.log("second largest elm(two pass): ", find2ndLargest_TwoPass(arr));
