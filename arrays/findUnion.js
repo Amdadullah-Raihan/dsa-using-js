f30beeea522b01edb479a46ab66c5616b1e70d93
Amdadul Islam <amdadullahrayhan@gmail.com>
2025-03-16 17:53:54 +0600

    add optimal implementation for finding the union of two arrays

--- arrays/findUnion.js
+++ arrays/findUnion.js
@@ -1,5 +1,5 @@
 //Brute force
-const findUnion = (arr1, arr2) => {
+const findUnion_BruteForce = (arr1, arr2) => {
   const unifiedSet = new Set();
 
   for (const item of arr1) {
@@ -19,4 +19,41 @@ const findUnion = (arr1, arr2) => {
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const arr2 = [2, 3, 4, 4, 5, 11, 12];
 
-console.log("union arr: ", findUnion(arr1, arr2));
+// console.log("union arr: ", findUnion_BruteForce(arr1, arr2));
+
+const findUnion_Optimal = (arr1, arr2) => {
+  let unionArr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      if (!unionArr.length || unionArr[unionArr.length - 1] !== arr1[i]) {
+        unionArr.push(arr1[i]);
+      }
+      i++;
+    } else {
+      if (!unionArr.length || unionArr[unionArr.length - 1] !== arr2[j]) {
+        unionArr.push(arr2[j]);
+      }
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    if (!unionArr.length || unionArr[unionArr.length - 1] !== arr1[i]) {
+      unionArr.push(arr1[i]);
+    }
+    i++;
+  }
+
+  while (j < arr2.length) {
+    if (!unionArr.length || unionArr[unionArr.length - 1] !== arr2[j]) {
+      unionArr.push(arr2[j]);
+    }
+    j++;
+  }
+
+  return unionArr;
+};
+
+console.log("union arr: ", findUnion_Optimal(arr1, arr2));
